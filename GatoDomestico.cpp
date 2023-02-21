f359594a4fe8bceec5236c7763c70003a3968e96
Jorge López <123602935+JorgeLpez@users.noreply.github.com>
2023-02-21 10:37:36 -0600

    Update GatoDomestico.cpp

--- GatoDomestico.cpp
+++ GatoDomestico.cpp
@@ -1,22 +1,55 @@
+#include <iostream>
 #include "GatoDomestico.h"
 
-GatoDomestico::GatoDomestico() : Dato(0)
+GatoDomestico::GatoDomestico() : Anio(0),Dieta(""),LugarNac(""),Raza("")
 {
     //ctor
 }
 
 GatoDomestico::GatoDomestico(GatoDomestico& G)
 {
-    Dato = G.Dato;
+    Anio = G.Anio;
+    Dieta = G.Dieta;
+    LugarNac = G.LugarNac;
+    Raza = G.Raza;
 }
 
-void GatoDomestico::setDato(const int& G)
+void GatoDomestico::setAnio(const int& G)
 {
-    Dato = G;
+    Anio = G;
 }
 
-
-int GatoDomestico::getDato() const
+void GatoDomestico::setDieta(const std::string& G)
 {
-  return Dato;
+    Dieta = G;
+}
+
+void GatoDomestico::setLugarNac(const std::string& G)
+{
+    LugarNac = G;
+}
+
+void GatoDomestico::setRaza(const std::string& G)
+{
+    Raza = G;
+}
+
+int GatoDomestico::getAnio() const
+{
+  return Anio;
+}
+
+std::string GatoDomestico::getDieta() const
+{
+  return Dieta;
+}
+
+std::string GatoDomestico::getLugarNac() const
+{
+  return LugarNac;
+}
+
+std::string GatoDomestico::getRaza() const
+{
+  return Raza;
 }
