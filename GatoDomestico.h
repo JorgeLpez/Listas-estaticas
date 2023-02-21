98de1ffd25750731ef3dea0c8aab837c72d75505
Jorge López <123602935+JorgeLpez@users.noreply.github.com>
2023-02-21 10:37:52 -0600

    Update GatoDomestico.h

--- GatoDomestico.h
+++ GatoDomestico.h
@@ -5,13 +5,21 @@
 class GatoDomestico
 {
     private:
-        int Dato;
+        int Anio;
+        std::string Dieta,LugarNac,Raza;
 
     public:
         GatoDomestico();
         GatoDomestico(GatoDomestico&);
-        int getDato() const;
-        void setDato(const int&);
+        int getAnio() const;
+        std::string getDieta() const;
+        std::string getLugarNac() const;
+        std::string getRaza() const;
+
+        void setAnio(const int&);
+        void setDieta(const std::string&);
+        void setLugarNac(const std::string&);
+        void setRaza(const std::string&);
 };
 
 #endif // GATODOMESTICO_H
